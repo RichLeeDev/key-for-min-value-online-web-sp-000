fa7880cb183813552576080458349b19e8a9f9b6
Richard Lee <richlee81192@gmail.com>
2019-09-23 22:54:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  beginning_number = 10000
+  name_hash.collect do |key, value|
+    if value < beginning_number
+      
+  end 
 end

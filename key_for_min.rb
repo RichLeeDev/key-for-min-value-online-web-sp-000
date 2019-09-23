01b5b4cabf9833b2df98731d6b4c26212dfd6d8e
Richard Lee <richlee81192@gmail.com>
2019-09-23 22:56:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,9 @@
 
 def key_for_min_value(name_hash)
   lowest_val = 0
-  lowest_key
-  name_hash.collect do |key, value|
-    if value < beginning_number
+  lowest_key = nil
+  name_hash.each do |key, value|
+    if lowest_val == 0 && value <
       
   end 
 end
